eea51df9e3de63b16ad973426176eb103035e2cd
Vasudeo-Rane <128340950+Vasudeo-Rane@users.noreply.github.com>
2024-04-06 10:15:29 -0400

    added new assignments on 6th April

---
+++ src/Apr06/Animal.java
@@ -0,0 +1,27 @@
+package Apr06;
+
+public class Animal {
+    String color;
+    double height;
+
+    Animal() {
+        color = "Red";
+        height = 9.99;
+    }
+
+    public void walk() {
+        System.out.println("Animal is walking and his color is " + color);
+    }
+
+    public void talk() {
+        System.out.println("Animal is talking his height is " + height);
+    }
+
+    public void eat() {
+        System.out.println("Animal is eating ");
+    }
+
+    public void makeSound() {
+        System.out.println("Animal is making Sound");
+    }
+}
---
+++ src/Apr06/AnimalMain.java
@@ -0,0 +1,13 @@
+package Apr06;
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.talk();
+        animal.walk();
+        animal.makeSound();
+        System.out.println("animal height is " + animal.height);
+        System.out.println("animal color is " + animal.color);
+    }
+}
