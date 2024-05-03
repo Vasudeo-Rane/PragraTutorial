677763084db0182f44e2ef71de48328a82033c8d
Vasudeo-Rane <128340950+Vasudeo-Rane@users.noreply.github.com>
2024-05-02 20:34:52 -0400

    added classes

---
+++ src/apr30/Bank.java
@@ -0,0 +1,7 @@
+package apr30;
+
+public class Bank {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/apr30/ExceptionClass.java
@@ -0,0 +1,27 @@
+package apr30;
+
+public class ExceptionClass {
+    public static void main(String[] args) {
+        int[] arr = new int[2];
+        arr[0] = 1;
+        arr[1] = 2;
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(arr[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String name = null;
+        try {
+            System.out.println(name.charAt(0));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
